716ca078d87f79f2d51c7c27343294a0e4cad2a4
Hotsse <dkdlrja@naver.com>
2019-09-28 16:42:17 +0900

    update authorize

--- src/main/java/com/daou/admin/manager/ManagerHomeController.java
+++ src/main/java/com/daou/admin/manager/ManagerHomeController.java
@@ -3,10 +3,14 @@ package com.daou.admin.manager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.daou.admin.common.annotation.AuthAction;
+import com.daou.admin.common.annotation.type.ActionType;
+
 @Controller
 @RequestMapping(value="/manager")
 public class ManagerHomeController {
 	
+	@AuthAction(action=ActionType.ADMIN)
 	@RequestMapping(value="")
 	public String home() throws Exception {
 		return "manager/home";
