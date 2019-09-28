f2f10595801b3df5936478a955d9a01f53429703
Hotsse <dkdlrja@naver.com>
2019-09-28 17:17:38 +0900

    apply lombok project

--- src/main/java/com/daou/admin/login/vo/MemberVO.java
+++ src/main/java/com/daou/admin/login/vo/MemberVO.java
@@ -17,47 +17,4 @@ public class MemberVO {
 	private int userRole;
 	private String isAdminYn;
 	private String currentIp;
-	
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getUserNm() {
-		return userNm;
-	}
-	public void setUserNm(String userNm) {
-		this.userNm = userNm;
-	}
-	public int getUserDept() {
-		return userDept;
-	}
-	public void setUserDept(int userDept) {
-		this.userDept = userDept;
-	}
-	public int getUserRole() {
-		return userRole;
-	}
-	public void setUserRole(int userRole) {
-		this.userRole = userRole;
-	}
-	public String getIsAdminYn() {
-		return isAdminYn;
-	}
-	public void setIsAdminYn(String isAdminYn) {
-		this.isAdminYn = isAdminYn;
-	}
-	public String getCurrentIp() {
-		return currentIp;
-	}
-	public void setCurrentIp(String currentIp) {
-		this.currentIp = currentIp;
-	}
-	
-	@Override
-	public String toString() {
-		return "MemberVO [userId=" + userId + ", userNm=" + userNm + ", userDept=" + userDept + ", userRole=" + userRole
-				+ ", isAdminYn=" + isAdminYn + ", currentIp=" + currentIp + "]";
-	}
 }
--- src/main/java/com/daou/admin/manager/role/vo/RoleVO.java
+++ src/main/java/com/daou/admin/manager/role/vo/RoleVO.java
@@ -14,22 +14,4 @@ public class RoleVO {
 
 	private int roleIdx;
 	private String roleNm;
-	
-	public int getRoleIdx() {
-		return roleIdx;
-	}
-	public void setRoleIdx(int roleIdx) {
-		this.roleIdx = roleIdx;
-	}
-	public String getRoleNm() {
-		return roleNm;
-	}
-	public void setRoleNm(String roleNm) {
-		this.roleNm = roleNm;
-	}
-	
-	@Override
-	public String toString() {
-		return "RoleVO [roleIdx=" + roleIdx + ", roleNm=" + roleNm + "]";
-	}
 }
